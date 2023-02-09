f96ad897ed396886e7e0bace36b174fcfaee6dcf
denis.shalimov <denis.shalimov@SHALIMOV-LT>
2023-02-09 20:27:03 +0300

    Добавьте файлы проекта.

---
+++ main/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        int cb = 1;
+        int[] zd2 = new int[15];
+        word(zd2, cb);
+
+
+    }
+
+    static void word(int[] a, int b)
+    {
+        for (int ac = 5; ac < 15; ac++)
+        {
+
+            Console.WriteLine(ac);
+
+
+            Console.WriteLine(a[ac]);
+        }
+    }
+
+}
